669ab85001d2a9c657d74de27c93458b8f941df2
Azeem-pdd <90024412+Azeem-pdd@users.noreply.github.com>
2021-09-16 19:45:46 +0500

    another commit

--- Estore/settings.py
+++ Estore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0_4$mzu555-$!qd5-%qhc-zk=8ykykyto^yaz9h9(gwj0#ex77'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','shopoutfitsonline.herokuapp.com']
 
 
 # Application definition
