d4115e2b22a0504baf9d24b030ed8bfd4763a5bd
Azeem-pdd <90024412+Azeem-pdd@users.noreply.github.com>
2021-09-18 14:06:22 +0500

    c

--- Estore/settings.py
+++ Estore/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
