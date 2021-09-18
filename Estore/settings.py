f7ddd58631ae75b235ae52748d2fa6a453d2761a
Azeem-pdd <90024412+Azeem-pdd@users.noreply.github.com>
2021-09-18 22:21:29 +0500

    c

--- Estore/settings.py
+++ Estore/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
