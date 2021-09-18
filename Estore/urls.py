736e0c9db575dec2f49070907688e6ecf0d0becf
Azeem-pdd <90024412+Azeem-pdd@users.noreply.github.com>
2021-09-18 14:27:28 +0500

    Update urls.py

--- Estore/urls.py
+++ Estore/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns = urlpatterns + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
