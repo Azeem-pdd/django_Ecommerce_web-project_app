a95d8c2f61798e1386745ffa229d9820675cdd69
Azeem-pdd <90024412+Azeem-pdd@users.noreply.github.com>
2021-09-21 21:31:02 +0500

    Update settings.py

--- Estore/settings.py
+++ Estore/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Estore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'onlinestore',
+        'NAME': 'storeapp',
         'USER':'postgres',
         'PASSWORD':'313313',
         'HOST':'localhost',
@@ -133,7 +133,7 @@ MEDIA_URL='/media/'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default']=dj_database_url.config(default='postgres://postgres:313313@localhost:5432/onlinestore')
+DATABASES['default']=dj_database_url.config(default='postgres://postgres:313313@localhost:5432/storeapp')
 DATABASES['default'].update(db_from_env)
 
 
