6f4cbec72638cf62e75c6602943dcfc35852a12d
Azeem-pdd <90024412+Azeem-pdd@users.noreply.github.com>
2021-09-22 16:40:52 +0500

    Update settings.py

--- Estore/settings.py
+++ Estore/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
